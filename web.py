9638aa2ac9b6cd4cb3d088c0f8ef503fca729479
017forever <carat17forever0713@gmail.com>
2026-04-21 11:38:40 +0800

    20260421v3

--- web.py
+++ web.py
@@ -42,7 +42,15 @@ def index():
 
 @app.route("/sp1")
 def sp1():
-    R="20260421"
+    R=""
+    url = "https://pyweb0414check.vercel.app/about"
+    Data = requests.get(url)
+    Data.encoding = "utf-8"
+    #print(Data.text)
+    sp = BeautifulSoup(Data.text, "html.parser")
+    result=sp.select("td a")
+    for item in result:
+        R+= item.text+"<br>"+item.get("href")+"<br><br>"
     return R
 
 @app.route("/search")
