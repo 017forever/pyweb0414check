0439ff81a20f6db474ea027a981e61b76df48e96
017forever <carat17forever0713@gmail.com>
2026-05-12 10:59:12 +0800

    webhook1

--- web.py
+++ web.py
@@ -19,7 +19,7 @@ else:
 
 firebase_admin.initialize_app(cred)
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, jsonify
 from datetime import datetime
 import random
 
@@ -44,6 +44,17 @@ def index():
     link += "<a href=/WT>天氣預報</a><hr>"
     link += "<a href=/rate>本週新片進DB</a><hr>"
     return link
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    # build a request object
+    req = request.get_json(force=True)
+    # fetch queryResult from json
+    action =  req.get("queryResult").get("action")
+    msg =  req.get("queryResult").get("queryText")
+    info = "動作：" + action + "； 查詢內容：" + msg
+    return make_response(jsonify({"fulfillmentText": info}))
+
 
 @app.route("/rate")
 def rate():
