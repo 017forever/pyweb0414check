a8c1357e0742dd364211b6f6b8b303f066272987
017forever <carat17forever0713@gmail.com>
2026-05-19 10:22:41 +0800

    Update web.py

--- web.py
+++ web.py
@@ -43,7 +43,7 @@ def index():
     link += "<a href=/road>JSON-十大肇事入口</a><hr>"
     link += "<a href=/WT>天氣預報</a><hr>"
     link += "<a href=/rate>本週新片進DB</a><hr>"
-    link += "<a href=/demo>demo進行</a><hr>"
+    link += "<a href=/demo>聊天機器人</a><hr>"
     return link
 
 @app.route("/demo")
